5338c7a3516fbb0b3bc45e64d6b246ac05366006
Héctor Tarascó Viciano <hectavi@pm.me>
2023-09-15 19:31:48 +0200

    Create about.inc.php

---
+++ includes/about.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Héctor Tarascó
+ * @version 23.09
+ */
+?>
+<section id='about'>
+    <h2>About me</h2>
+    <img src='/home/hectarvic/lamp/apache2/htdocs/img/photo.webp' alt='Héctor Tarascó'>
+    <p>Welcome to my portfolio!</p>
+</section>

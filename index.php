73c7a048fd500e62361759f7527419c9f9ac79b4
Héctor Tarascó Viciano <hectavi@pm.me>
2023-09-15 19:29:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php
 /**
  * @author Héctor Tarascó
- * @version 23.09
+ * @version 23.09.1
  */
 require_once(__DIR__ . '/includes/header.inc.php');
 ?>
@@ -11,16 +11,16 @@ require_once(__DIR__ . '/includes/header.inc.php');
 <body>
     <?php
     require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/nav.inc.php');
-
-    echo "<div id='content'>";
-
-    require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/about.inc.php');
-    require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/resume.inc.php');
-    require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/projects.inc.php');
-    require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/hobbies.inc.php');
-
-    echo "</div>";
-
+    ?>
+    <div id='content'>
+        <?php
+        require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/about.inc.php');
+        require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/resume.inc.php');
+        require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/projects.inc.php');
+        require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/hobbies.inc.php');
+        ?>
+    </div>
+    <?php
     require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/includes/footer.inc.php');
     ?>
 </body>

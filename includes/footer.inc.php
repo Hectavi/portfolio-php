7d3d0658d391ab31a388b9e1e989ed3f70c4c106
Héctor Tarascó Viciano <hectavi@pm.me>
2023-09-15 19:09:52 +0200

    Update footer.inc.php

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,7 +1,11 @@
 <?php
+/**
+ * @author Héctor Tarascó
+ * @version 23.09
+ */
 require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/index.php');
 ?>
 
 <footer>
-
+    <p>© 2023 Héctor Tarascó</p>
 </footer>

893e857dd53d4fdd9ee29032d2b42ec7d5a9979d
Héctor Tarascó Viciano <hectavi@pm.me>
2023-09-15 19:08:26 +0200

    Update header.inc.php

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,10 +1,14 @@
 <?php
+/**
+ * @author Héctor Tarascó
+ * @version 23.09
+ */
 require_once(__DIR__ . '/home/hectarvic/lamp/apache2/htdocs/index.php');
 ?>
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" type='text/css' href="style.css">
     <title>Portfolio</title>
 </head>
